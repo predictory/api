d3d1e415d73a16ab928b9758e6fa387f4eb7088a
Fir3st <fojtik.v@gmail.com>
2020-02-12 06:05:42 +0100

    Added admin guard to protect routes

--- src/modules/users/users.controller.ts
+++ src/modules/users/users.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { UsersService } from './users.service';
 import { CurrentUser } from './current-user.decorator';
+import { AdminGuard } from '@modules/auth/admin.guard';
 import { UserRO } from './dtos';
 
 @Controller('users')
@@ -9,6 +10,7 @@ export class UsersController {
     constructor(private readonly usersService: UsersService) {}
 
     @Get()
+    @UseGuards(AuthGuard('jwt'), AdminGuard)
     async findAll(): Promise<UserRO[]> {
         return this.usersService.findAll();
     }
